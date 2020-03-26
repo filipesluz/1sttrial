9b88c8c4756ae092c697e8286fb120eba83b065b
filipesluz <filipesluz@users.noreply.github.com>
2020-03-26 23:33:35 +0000

    v1.0

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const TeleBot = require('telebot');
+const bot = new TeleBot('951062963:AAEhjPJWd-CtepUwcZ8yD5LWlYDxQ8CMMCM');
+
+bot.on('text', (msg) => msg.reply.text(msg.text));
+
+bot.start();
