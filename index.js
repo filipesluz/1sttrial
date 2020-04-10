1ae651693de01f781095f180dd6576c1d78146ab
filipesluz <filipesluz@users.noreply.github.com>
2020-04-10 15:52:36 +0100

    commit on master

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const bot = new TeleBot('951062963:AAEhjPJWd-CtepUwcZ8yD5LWlYDxQ8CMMCM');
 bot.on('message', (msg) => {
 var cu = "cu";
 var merda = "merda";
-var bye = "adeus";
-var Hi = "ola";
+var bye = ["adeus","bye","xau","até logo","vou embora"];
+var Hi = ["ola","bom dia","boa tarde"];
+var tudobem = ["então tudo bem?","tá tudo?","tudo bem?"];
 
 if (msg.text.toString().toLowerCase().indexOf(cu) === 0) {
     bot.sendMessage(msg.from.id, "Mete mas é o dedo no teu cu, oh " + msg.from.first_name);
@@ -19,6 +20,9 @@ if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
 }
 if (msg.text.toString().toLowerCase().includes(bye)) {
 bot.sendMessage(msg.chat.id, "Ponha-se no caralho Sr " + msg.from.first_name);
+}
+if (msg.text.toString().toLowerCase().indexOf(tudobem) === 0) {
+    bot.sendMessage(msg.from.id, "Tá tudo na merda mas vamos andando");
 }
 });
 bot.start();
